e414a5e1cce6a24fd04b474703e02a5ddda09d74
lilong5 <lilong5@kingsoft.com>
2024-08-21 17:22:53 +0800

    custom plugins安装lualine,bufferline,nvim-tree plugins

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,53 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'nvim-lualine/lualine.nvim',
+    config = function()
+      require('lualine').setup {}
+    end,
+  },
+  -- bufferline
+  {
+    'akinsho/bufferline.nvim',
+    version = '*',
+    dependencies = 'nvim-tree/nvim-web-devicons',
+    config = function()
+      require('bufferline').setup {
+        options = {
+          close_command = 'bdelete! %d', -- 点击关闭按钮关闭
+          right_mouse_command = 'bdelete! %d', -- 右键点击关闭
+          indicator = {
+            icon = '▎', -- 分割线
+            style = 'underline',
+          },
+          offsets = {
+            {
+              filetype = 'NvimTree',
+              text = 'File Explorer',
+              text_align = 'left',
+              separator = true,
+            },
+          },
+        },
+      }
+    end,
+  },
+  {
+    'nvim-tree/nvim-tree.lua',
+    version = '*',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      require('nvim-tree').setup {
+        sort_by = 'case_sensitive',
+        renderer = {
+          group_empty = true,
+        },
+        filters = {
+          dotfiles = true,
+        },
+      }
+    end,
+  },
+}
